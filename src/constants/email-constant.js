9772b25c3df867c8ddcb660f8fa0e94aca3b009c
heru-a11y <heru@bangunindo.com>
2026-02-25 19:44:15 +0700

    move template email en & id

--- src/constants/email-constant.js
+++ src/constants/email-constant.js
@@ -3,11 +3,11 @@ export const LOG_PREFIX = "[Email Service]";
 export const EMAIL_CONFIG = {
     VERIFICATION: {
         id: {
-            templateFile: "verification-email.html",
+            templateFile: "id/verification-email.html", 
             subject: "Kode Verifikasi (OTP) - Snap Journal",
         },
         en: {
-            templateFile: "verification-email-en.html",
+            templateFile: "en/verification-email.html", 
             subject: "Verification Code (OTP) - Snap Journal",
         },
         logName: "Verification OTP",
@@ -15,11 +15,11 @@ export const EMAIL_CONFIG = {
     },
     RESET_PASSWORD: {
         id: {
-            templateFile: "reset-password.html",
+            templateFile: "id/reset-password.html",
             subject: "Permintaan Reset Password - Snap Journal",
         },
         en: {
-            templateFile: "reset-password-en.html",
+            templateFile: "en/reset-password.html",
             subject: "Password Reset Request - Snap Journal",
         },
         logName: "Reset Password OTP",
@@ -27,11 +27,11 @@ export const EMAIL_CONFIG = {
     },
     UPDATE_PASSWORD: {
         id: {
-            templateFile: "update-password.html",
+            templateFile: "id/update-password.html",
             subject: "Keamanan Akun: Kode Verifikasi Ganti Password - Snap Journal",
         },
         en: {
-            templateFile: "update-password-en.html",
+            templateFile: "en/update-password.html",
             subject: "Account Security: Change Password Verification Code - Snap Journal",
         },
         logName: "Update Password OTP",
@@ -39,11 +39,11 @@ export const EMAIL_CONFIG = {
     },
     CHANGE_EMAIL: {
         id: {
-            templateFile: "change-email.html",
+            templateFile: "id/change-email.html",
             subject: "Konfirmasi Perubahan Email - Snap Journal",
         },
         en: {
-            templateFile: "change-email-en.html",
+            templateFile: "en/change-email.html",
             subject: "Email Change Confirmation - Snap Journal",
         },
         logName: "Change Email OTP",
@@ -51,11 +51,11 @@ export const EMAIL_CONFIG = {
     },
     DELETE_ACCOUNT: {
         id: {
-            templateFile: "delete-account.html",
+            templateFile: "id/delete-account.html",
             subject: "PENTING: Kode Konfirmasi Penghapusan Akun - Snap Journal",
         },
         en: {
-            templateFile: "delete-account-en.html",
+            templateFile: "en/delete-account.html",
             subject: "IMPORTANT: Account Deletion Confirmation Code - Snap Journal",
         },
         logName: "Delete Account OTP",
